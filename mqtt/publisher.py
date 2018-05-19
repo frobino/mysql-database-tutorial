ec67d5cf27113c5068d4f5b7919b93008df9976e
Francesco Robino <osso@T440p.bredbandsbolaget.se>
2018-05-19 13:56:45 +0200

    mqtt: added documentation

--- mqtt/publisher.py
+++ mqtt/publisher.py
@@ -1,10 +1,30 @@
+# Requires Eclipse Paho MQTT Python client library, which implements the MQTT protocol.
+# The MQTT protocol is a machine-to-machine (M2M)/Internet of Things connectivity protocol.
+# Designed as an extremely lightweight publish/subscribe messaging transport,
+# it is useful for connections with remote locations where a small code footprint is required
+# and/or network bandwidth is at a premium.
+#
+# pip install paho-mqtt
+#
+# This code provides a client class which enable applications to connect to an MQTT broker
+# to publish messages, and to subscribe to topics and receive published messages.
+# 
+#                +----+
+#                |    |
+#                |    |
+#               /+----+ broker (receiving from publisher client)
+#      +----+  /
+#      |    | /
+#      |    |/
+#      +----+ publisher (sending to broker)
+
 import paho.mqtt.client as mqtt #import the client1
 import time
 
 broker_address="broker.hivemq.com"
 
 client = mqtt.Client("P1") #create new instance
-print("connecting to broker")
+print("Connecting to broker")
 client.connect(broker_address) #connect to broker
 print("Publishing message to topic","house/bulbs/bulb1")
 i=0
--- mqtt/subscriber.py
+++ mqtt/subscriber.py
@@ -1,3 +1,24 @@
+# Requires Eclipse Paho MQTT Python client library, which implements the MQTT protocol.
+# The MQTT protocol is a machine-to-machine (M2M)/Internet of Things connectivity protocol.
+# Designed as an extremely lightweight publish/subscribe messaging transport,
+# it is useful for connections with remote locations where a small code footprint is required
+# and/or network bandwidth is at a premium.
+#
+# pip install paho-mqtt
+#
+# This code provides a client class which enable applications to connect to an MQTT broker
+# to publish messages, and to subscribe to topics and receive published messages.
+#
+#       +----+
+#       |    |
+#       |    |
+#       +----+ broker (receiving from publisher client, forwarding to subscriber client)
+#           \-     +----+
+#             \-   |    |
+#               \- |    |
+#                 \+----+ subscriber (receiving from broker)
+
+
 import paho.mqtt.client as mqtt #import the client1
 import time
 
@@ -8,7 +29,6 @@ def on_message(client, userdata, message):
     print("message retain flag=",message.retain)
 
 broker_address="broker.hivemq.com"
-
 
 client = mqtt.Client("S1") #create new instance
 print("connecting to broker")
